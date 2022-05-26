c1f38562ee4b965ba61e79cac282c08653d3fd3a
Dev Raj <rehansingh.4522@gmail.com>
2022-05-26 23:58:55 +0530

    initial commit

---
+++ frontend/src/theme.js
@@ -0,0 +1,16 @@
+import { orange } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+
+export const theme = {
+  spacing: 10,
+  palette: {
+    primary: {
+      main: '#2596be',
+    },
+  },
+
+  typography: {
+    fontFamily: 'Roboto',
+    fontWeight: '600',
+  },
+};
