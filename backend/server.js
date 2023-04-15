ea04102475a7fcbe8a63b5f8507beb3dd1144997
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2023-04-15 18:56:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,6 @@ app.use(express.json());
 // Set CORS headers for all responses
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
